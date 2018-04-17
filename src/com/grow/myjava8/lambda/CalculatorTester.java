951d3254f88ba4318b08c7fbbcc5fea7b94ced72
KVO <kvo@ukr.net>
2018-04-18 00:13:49 +0300

    added Recursion

--- src/com/grow/myjava8/lambda/CalculatorTester.java
+++ src/com/grow/myjava8/lambda/CalculatorTester.java
@@ -1,10 +1,12 @@
 package com.grow.myjava8.lambda;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class CalculatorTester {
     private Calculator calculator = new Calculator();
+    private Function<Double, Double> factorial = n -> n > 1d ? this.factorial.apply(n - 1) * n : 1d;
 
     CalculatorTester(){
         initOperatuions();
@@ -32,6 +34,7 @@ public class CalculatorTester {
         calculator.addOperation("abs", Math::abs);
         calculator.addOperation("sin", Math::sin);
         calculator.addOperation("cos", Math::cos);
+        calculator.addOperation("!", factorial);
     }
 
     private void randomCalculstor(String operator){
@@ -39,7 +42,7 @@ public class CalculatorTester {
         final String line;
         if (calculator.isOperator(operator)){
             operands = new Double[1];
-            operands[0] = randomDouble();
+            operands[0] = !operator.equals("!") ? randomDouble() : randomRounDouble();
             line = String.format("%s(%.2f) = ",operator, operands[0]);
         }else{
             operands = new Double[2];
@@ -48,12 +51,15 @@ public class CalculatorTester {
             line = String.format("%.2f %s %.2f = ",operands[0], operator, operands[1]);
         }
 
-        calculator.calculate(operator, operands);
         System.out.println(line + calculator.calculate(operator, operands));
     }
 
     private static Double randomDouble(){
         return Math.round(Math.random() * 100d) / 10d;
+    }
+
+    private static Double randomRounDouble(){
+        return (double) Math.round(Math.random() * 10);
     }
 
 }
--- src/com/grow/myjava8/lambda/LambdaMain.java
+++ src/com/grow/myjava8/lambda/LambdaMain.java
@@ -3,7 +3,7 @@ package com.grow.myjava8.lambda;
 public class LambdaMain {
     public static void main(String[] args) {
         CalculatorTester calculatorTester = new CalculatorTester();
-        calculatorTester.doTest(100);
+        calculatorTester.doTest(10000);
     }
 }
 
